ab502f4177c91aeff847b9d60b1e11a458edeea5
FlatThree <xenderbynx@gmail.com>
2020-10-26 16:21:22 +0000

    Add life.py and create class Animal

---
+++ life.py
@@ -0,0 +1,9 @@
+ # I'll add to this later
+class Animal:
+  def __init__(self, name, diet, energy=100, repro=150, fatigue=10):
+    self.name = name
+    self.kingdom = 'Animal'
+    self.energy = energy
+    self.diet = diet
+    self.repro = repro
+    self.fatigue = fatigue
